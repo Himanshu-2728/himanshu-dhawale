cf4c234468aafa596301ff8e4dd28a7389ffaf0d
Himanshu-2728 <imunknown2728@gmail.com>
2026-06-26 14:41:48 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { FaRegFileAlt } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
+import { TbBrandLinkedin } from "react-icons/tb";
+import { FaInstagram } from "react-icons/fa";
+
+export default function App() {
+  return (
+    <>
+      <div className="bg-[#2C2C2A] flex justify-around h-16 items-center border-b-[0.5px] border-gray-600 sticky top-0 py-10">
+        <p className="text-white font-bold mx-10">
+          {"{ himanshu.dev }"}
+        </p>
+        <div className="text-[#A4A49A] mx-10 font-poppins flex justify-around w-[25%]">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#skills">Skills</a>
+          <a href="#contact">Contact</a>
+        </div>
+      </div>
+      <section id="home" className="h-[100vh] text-white flex items-center justify-center">
+        <div>
+          <h1 className="text-center text-7xl font-poppins">
+            Himanshu Dhawale. 
+          </h1>
+          <p className="text-2xl font-mono mt-6">
+            AIML Student passionate about NLP, Computer Vision and building deployable ML applications.
+          </p>
+          <div className="mt-5 text-center flex justify-center ">
+            <button className="hover:border-blue-600 flex items-center bg-[#2C2C2A]  rounded-lg p-2 border-[0.5px] border-gray-600 mx-1">
+              <FaRegFileAlt className="mr-1" />
+                Resume
+            </button> 
+            <button className="hover:border-blue-600 flex items-center bg-[#2C2C2A]  rounded-lg p-2 border-[0.5px] border-gray-600 mx-1"> 
+              <FiGithub className="mr-1" />
+              Github
+            </button>
+            <button className="hover:border-blue-600 flex items-center bg-[#2C2C2A]  rounded-lg p-2 border-[0.5px] border-gray-600 mx-1">
+              <TbBrandLinkedin className="mr-1" />
+              LinkedIn
+            </button>
+            <button className="hover:border-blue-600 flex items-center bg-[#2C2C2A]  rounded-lg p-2 border-[0.5px] border-gray-600 mx-1">
+              <FaInstagram  className="mr-1"/>
+              Instagram
+            </button>
+          </div>
+        </div>
+      </section>
+      <section id="about" className="h-[100vh]">
+        <div className="text-white p-28">
+          <div>
+            <h1 className="text-4xl font-poppins">
+              About Me
+            </h1>
+            <p className="mt-5 font-mono">
+              I'm Himanshu, a developer with a strong interest in Machine Learning and Artificial Intelligence, focused on building practical applications that solve real-world problems. <br />
+
+              I enjoy taking projects from idea to deployment—training machine learning models, developing backend APIs, and creating intuitive web interfaces that make those models accessible to users. Through projects in natural language processing, computer vision, and full-stack development, I've gained hands-on experience with model evaluation, feature engineering, deployment, and building end-to-end ML applications.
+            </p>
+          </div>
+          <div>
+            <h1 className="text-4xl font-poppins mt-10">
+              Education
+            </h1>
+            <table className="mt-5 font-mono">
+              <tbody>
+                <tr>
+                  <td>
+                    BTech - Artificial Intelligence And Machine Learning
+                  </td>
+                  <td>
+                    <span className="bg-blue-600 p-1 rounded-md mx-5">Persuing</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                     Ycce - Yeshvantrao Chavan College of Engineering
+                  </td>
+                  <td>
+                    <span className="mx-5 text-blue-500">Sept 2025 - Present</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
